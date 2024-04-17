359b631ee7676f51fdd0278f3d363863c20a4b02
melvinousephjacob <119927690+melvinousephjacob@users.noreply.github.com>
2024-04-17 14:10:03 +0530

    Update itracelogger.h

--- examples/logs_simple/itracelogger.h
+++ examples/logs_simple/itracelogger.h
@@ -1,1 +1,15 @@
-x
+#pragma once
+#include "tracedata.h";
+#include <string>
+#include <exception>
+
+class ITraceLogger
+{
+    public:
+        void TraceInfo(std::string message, std::exception exception);
+        void TraceInfo(std::string message, TraceData data);
+        void TraceInfo(std::string message);
+        void TraceVerbose(std::string message);
+        void TraceVerbose(std::string message, TraceData data);
+        void TraceVerbose(std::string message, std::exception exception);
+};
