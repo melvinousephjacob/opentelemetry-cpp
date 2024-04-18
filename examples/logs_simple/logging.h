478712a1368b0960d897d362655f9b3b27e001a3
melvinousephjacob <119927690+melvinousephjacob@users.noreply.github.com>
2024-04-18 16:42:16 +0530

    Update logging.h

--- examples/logs_simple/logging.h
+++ examples/logs_simple/logging.h
@@ -29,18 +29,8 @@ namespace logging
 	{
 		otel::CleanupLogger();
 	}
-	
-	void CentralLogServer::Log(std::string message, Severity severity, CoreLogData coreLogData)
-	{
-		//auto logger = otel::get_logger(_moduleName, _version);
-	
-		std::unordered_map<std::string, std::string> mymap;
-		//mymap["NameSpace"] = coreLogData.Namespace;
-			
-		LogWithSeverity(logger, severity, mymap, message);
-	}
 
-	void LogWithSeverity(nostd::shared_ptr<logs::Logger> logger, Severity severity, std::unordered_map<std::string, std::string> mymap, std::string message)
+	void CentralLogServer::LogWithSeverity(nostd::shared_ptr<logs::Logger> logger, Severity severity, std::unordered_map<std::string, std::string> mymap, std::string message)
 	{
 		if(severity == Info)
 			logger->Info(message, mymap);
@@ -54,6 +44,16 @@ namespace logging
 			logger->Debug(message, mymap);
 	}
 	
+	void CentralLogServer::Log(std::string message, Severity severity, CoreLogData coreLogData)
+	{
+		//auto logger = otel::get_logger(_moduleName, _version);
+	
+		std::unordered_map<std::string, std::string> mymap;
+		//mymap["NameSpace"] = coreLogData.Namespace;
+			
+		CentralLogServer::LogWithSeverity(logger, severity, mymap, message);
+	}
+	
 	void CentralLogServer::Log(std::string message, Severity severity, std::exception exception, CoreLogData coreLogData)
 	{
 		//auto logger = otel::get_logger(_moduleName, _version);
@@ -61,7 +61,7 @@ namespace logging
 		std::unordered_map<std::string, std::string> mymap;
 		mymap["ExceptionInfo"] = exception.what();
 
-		LogWithSeverity(logger, severity, mymap, message);
+		CentralLogServer::LogWithSeverity(logger, severity, mymap, message);
 	}
 	
 	void CentralLogServer::Log(std::string message, Severity severity, InfoCategory infoCategory, CoreLogData coreLogData)
@@ -71,7 +71,7 @@ namespace logging
 		std::unordered_map<std::string, std::string> mymap;
 		mymap["InfoCategory"] = std::to_string(infoCategory);
 
-		LogWithSeverity(logger, severity, mymap, message);
+		CentralLogServer::LogWithSeverity(logger, severity, mymap, message);
 	}
 
 }  // namespace
