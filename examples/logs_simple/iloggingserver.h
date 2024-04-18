e1061845ef0e3eca97feab4e4d4995342e31bf0b
melvinousephjacob <119927690+melvinousephjacob@users.noreply.github.com>
2024-04-18 15:14:18 +0530

    Update iloggingserver.h

--- examples/logs_simple/iloggingserver.h
+++ examples/logs_simple/iloggingserver.h
@@ -9,6 +9,4 @@ class ICentralLogServer
         void Log(std::string message, Severity severity, CoreLogData coreLogData);
         void Log(std::string message, Severity severity, std::exception exception, CoreLogData coreLogData);
         void Log(std::string message, Severity severity, InfoCategory infoCategory, CoreLogData coreLogData);
-
-        void LogWithSeverity(nostd::shared_ptr<logs::Logger> logger, Severity severity, std::unordered_map<std::string, std::string> mymap, std::string message);
 };
