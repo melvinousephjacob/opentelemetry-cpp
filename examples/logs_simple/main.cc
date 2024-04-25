5576fe9fbd014f65cb89a8d1eecef014e0a41252
melvinousephjacob <119927690+melvinousephjacob@users.noreply.github.com>
2024-04-25 15:33:43 +0530

    Update main.cc

--- examples/logs_simple/main.cc
+++ examples/logs_simple/main.cc
@@ -5,15 +5,15 @@
 
 int main()
 {
-  //auto centralLogServer1 = new logging::CentralLogServer("Logger #1", "Sample Namespace #1", "Sample Classname #1");
-  //auto centralLogServer2 = new logging::CentralLogServer("Logger #1", "Sample Namespace #2", "Sample Classname #2");
-  //auto centralLogServer3 = new logging::CentralLogServer("Logger #1", "Sample Namespace #1", "Sample Classname #1");
-  //CoreLogData coreLogData;
-  //coreLogData.Namespace = "Sample Namespace";
+  auto centralLogServer1 = new logging::CentralLogServer("Logger #1", "Sample Namespace #1", "Sample Classname #1");
+  auto centralLogServer2 = new logging::CentralLogServer("Logger #1", "Sample Namespace #2", "Sample Classname #2");
+  auto centralLogServer3 = new logging::CentralLogServer("Logger #1", "Sample Namespace #1", "Sample Classname #1");
+  CoreLogData coreLogData;
+  coreLogData.Namespace = "Sample Namespace";
   
-  //centralLogServer1->Log("This is a sample log message", DebugInfo, coreLogData);
-  //centralLogServer2->Log("HELLO", Fatal, coreLogData);
-  //centralLogServer3->Log("Hiiii", Error, OSEvent, coreLogData);
+  centralLogServer1->Log("This is a sample log message", DebugInfo, coreLogData);
+  centralLogServer2->Log("HELLO", Fatal, coreLogData);
+  centralLogServer3->Log("Hiiii", Error, OSEvent, coreLogData);
 
   auto traceLogger = new trace::TraceLogger("Sample module");
   TraceData traceData;
