71b03b171573af2ff55256e4248e1601181fc887
melvinousephjacob <119927690+melvinousephjacob@users.noreply.github.com>
2024-05-06 16:02:01 +0530

    Update main.cc

--- examples/otlp/main.cc
+++ examples/otlp/main.cc
@@ -7,10 +7,20 @@ int main()
 {
   trace_opts.url  = "http://opentelemetry-collector-lwshost:4318/v1/traces";
   logger_opts.url = "http://opentelemetry-collector-lwshost:4318/v1/logs";
-  auto centralLogServer = new CentralLogServer;
+  
+  auto centralLogServer1 = new logging::CentralLogServer("Logger #1", "Sample Namespace #1", "Sample Classname #1");
+  auto centralLogServer2 = new logging::CentralLogServer("Logger #1", "Sample Namespace #2", "Sample Classname #2");
+  auto centralLogServer3 = new logging::CentralLogServer("Logger #1", "Sample Namespace #1", "Sample Classname #1");
   CoreLogData coreLogData;
-  coreLogData.Resolution = "Example core log data";
+  coreLogData.Namespace = "Sample Namespace";
   
-  centralLogServer->Log("HELLO", Fatal, coreLogData);
-  
+  centralLogServer1->Log("This is a sample log message", DebugInfo, coreLogData);
+  centralLogServer2->Log("HELLO", Fatal, coreLogData);
+  centralLogServer3->Log("Hiiii", Error, OSEvent, coreLogData);
+
+  //auto traceLogger = new trace::TraceLogger("Sample module");
+  //TraceData traceData;
+  //traceData.AdditionalInfo = "This is some additional info.";
+  //traceData.DateTime = std::time(0);
+  //traceLogger->TraceInfo("Hey", traceData);
 }
