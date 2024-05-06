1091e8ccb334f782133469b632f48c48da449db2
melvinousephjacob <119927690+melvinousephjacob@users.noreply.github.com>
2024-05-06 15:46:06 +0530

    Create tracedata.h

---
+++ examples/otlp/tracedata.h
@@ -0,0 +1,18 @@
+
+#pragma once
+#include <string>
+#include <list>
+#include <utility>
+#include <ctime>
+#include <map>
+
+struct TraceData
+{
+    std::string AdditionalInfo;
+    std::string ContextInfo;
+    std::string EventType;
+    std::string StackTrace;
+    std::string ExceptionInfo;
+    std::time_t DateTime; 
+};
+ 
