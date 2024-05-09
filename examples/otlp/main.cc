a4d28c60ab0d00e3e1ee83528123ecdfef828de9
melvinousephjacob <119927690+melvinousephjacob@users.noreply.github.com>
2024-05-09 15:07:59 +0530

    Update main.cc

--- examples/otlp/main.cc
+++ examples/otlp/main.cc
@@ -14,16 +14,14 @@ int main()
   auto centralLogServer1 = new logging::CentralLogServer("Logger #1", "Sample Namespace #1", "Sample Classname #1", logger_opts);
   auto centralLogServer2 = new logging::CentralLogServer("Logger #1", "Sample Namespace #2", "Sample Classname #2", logger_opts);
   auto centralLogServer3 = new logging::CentralLogServer("Logger #1", "Sample Namespace #1", "Sample Classname #1", logger_opts);
-  CoreLogData coreLogData;
-  coreLogData.Namespace = "Sample Namespace";
   
-  centralLogServer1->Log("This is a sample log message", DebugInfo, coreLogData);
-  centralLogServer2->Log("HELLO", Fatal, coreLogData);
-  centralLogServer3->Log("Hiiii", Error, OSEvent, coreLogData);
+  centralLogServer1->Log("This is a sample log message with Debug severity", DebugInfo);
+  centralLogServer2->Log("This is a sample log message with Fatal severity", Fatal);
+  centralLogServer3->Log("This is a sample log message with Error severity and OSEvent info category", Error, OSEvent);
 
   auto traceLogger = new traces::TraceLogger("Sample module", trace_opts);
   TraceData traceData;
   traceData.AdditionalInfo = "This is some additional info.";
   traceData.DateTime = std::time(0);
-  traceLogger->TraceInfo("Hey", traceData);
+  traceLogger->TraceInfo("This is a sample trace message", traceData);
 }
