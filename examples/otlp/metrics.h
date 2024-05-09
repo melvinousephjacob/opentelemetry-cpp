4ed86d8b9dbbbc048705dbe522eb1c9d081434f7
melvinousephjacob <119927690+melvinousephjacob@users.noreply.github.com>
2024-05-09 16:59:57 +0530

    Update metrics.h

--- examples/otlp/metrics.h
+++ examples/otlp/metrics.h
@@ -1,1 +1,13 @@
+#include "imetricslogger.h"
 
+namespace metrics
+{
+  class MetricsLogger : public IMetricsLogger
+	{
+		public:
+			MetricsLogger(std::string loggerName, std::string nameSpace, std::string className, opentelemetry::exporter::otlp::OtlpHttpMetricExporterOptions metrics_opts);
+	
+			~MetricsLogger();
+	
+			void Log(std::string textStr);
+	};
