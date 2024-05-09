60756d6eb0dbe2062ee0e9fe89df3c63811afd57
melvinousephjacob <119927690+melvinousephjacob@users.noreply.github.com>
2024-05-09 15:12:12 +0530

    Update main.cc

--- examples/logs_simple/main.cc
+++ examples/logs_simple/main.cc
@@ -8,16 +8,14 @@ int main()
   auto centralLogServer1 = new logging::CentralLogServer("Logger #1", "Sample Namespace #1", "Sample Classname #1");
   auto centralLogServer2 = new logging::CentralLogServer("Logger #1", "Sample Namespace #2", "Sample Classname #2");
   auto centralLogServer3 = new logging::CentralLogServer("Logger #1", "Sample Namespace #1", "Sample Classname #1");
-  CoreLogData coreLogData;
-  coreLogData.Namespace = "Sample Namespace";
   
-  centralLogServer1->Log("This is a sample log message", DebugInfo, coreLogData);
-  centralLogServer2->Log("HELLO", Fatal, coreLogData);
-  centralLogServer3->Log("Hiiii", Error, OSEvent, coreLogData);
+  centralLogServer1->Log("This is a sample log message with Debug severity", DebugInfo);
+  centralLogServer2->Log("This is a sample log message with Fatal severity", Fatal);
+  centralLogServer3->Log("This is a sample log message with Error severity and OSEvent info category", Error, OSEvent);
 
-  //auto traceLogger = new trace::TraceLogger("Sample module");
-  //TraceData traceData;
-  //traceData.AdditionalInfo = "This is some additional info.";
-  //traceData.DateTime = std::time(0);
-  //traceLogger->TraceInfo("Hey", traceData);
+  auto traceLogger = new traces::TraceLogger("Sample module", trace_opts);
+  TraceData traceData;
+  traceData.AdditionalInfo = "This is some additional info.";
+  traceData.DateTime = std::time(0);
+  traceLogger->TraceInfo("This is a sample trace message", traceData);
 }
