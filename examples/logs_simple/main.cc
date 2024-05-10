7b4f3b8495b3edebe4d0f939762544a86fa3e508
melvinousephjacob <119927690+melvinousephjacob@users.noreply.github.com>
2024-05-10 16:29:24 +0530

    Update main.cc

--- examples/logs_simple/main.cc
+++ examples/logs_simple/main.cc
@@ -9,13 +9,13 @@ int main()
   auto centralLogServer2 = new logging::CentralLogServer("Logger #1", "Sample Namespace #2", "Sample Classname #2");
   auto centralLogServer3 = new logging::CentralLogServer("Logger #1", "Sample Namespace #1", "Sample Classname #1");
   
-  centralLogServer1->Log("This is a sample log message with Debug severity", DebugInfo);
-  centralLogServer2->Log("This is a sample log message with Fatal severity", Fatal);
-  centralLogServer3->Log("This is a sample log message with Error severity and OSEvent info category", Error, OSEvent);
+  centralLogServer1->Log("This is a sample non instrumented log message with Debug severity", DebugInfo);
+  centralLogServer2->Log("This is a sample non instrumented log message with Fatal severity", Fatal);
+  centralLogServer3->Log("This is a sample non instrumented log message with Error severity and OSEvent info category", Error, OSEvent);
 
   auto traceLogger = new trace::TraceLogger("Sample module");
   TraceData traceData;
   traceData.AdditionalInfo = "This is some additional info.";
   traceData.DateTime = std::time(0);
-  traceLogger->TraceInfo("This is a sample trace message", traceData);
+  traceLogger->TraceInfo("This is a sample non instrumented trace message", traceData);
 }
