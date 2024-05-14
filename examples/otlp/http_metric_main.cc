05003357c619cc12e15b1643532fc612192f903f
melvinousephjacob <119927690+melvinousephjacob@users.noreply.github.com>
2024-05-14 17:37:53 +0530

    Update http_metric_main.cc

--- examples/otlp/http_metric_main.cc
+++ examples/otlp/http_metric_main.cc
@@ -10,40 +10,7 @@
 
 int main(int argc, char *argv[])
 {
-  //std::string example_type;
-  /*if (argc > 1)
-  {
-    exporter_options.url = argv[1];
-  }
-
-  if (argc > 2)
-  {
-    example_type = argv[2];
-  }
-
-  if (argc > 3)
-  {
-    std::string debug              = argv[3];
-    exporter_options.console_debug = debug != "" && debug != "0" && debug != "no";
-  }
-
-  if (argc > 4)
-  {
-    std::string binary_mode = argv[4];
-    if (binary_mode.size() >= 3 && binary_mode.substr(0, 3) == "bin")
-    {
-      exporter_options.content_type = otlp_exporter::HttpRequestContentType::kBinary;
-    }
-  }
-
-  if (exporter_options.console_debug)
-  {
-    internal_log::GlobalLogHandler::SetLogLevel(internal_log::LogLevel::Debug);
-  }
-
-  exporter_options.console_debug = true;
-  internal_log::GlobalLogHandler::SetLogLevel(internal_log::LogLevel::Debug);*/
-
+  opentelemetry::exporter::otlp::OtlpHttpMetricExporterOptions exporter_options;
   exporter_options.url = "http://opentelemetry-collector-lwshost:4318/v1/metrics";
   std::string example_type{"all"};
   // Removing this line will leave the default noop MetricProvider in place.
