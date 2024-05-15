94c5021b420fe81e8c901d2bd7459c42ec65a592
melvinousephjacob <119927690+melvinousephjacob@users.noreply.github.com>
2024-05-15 16:56:53 +0530

    Update metrics.h

--- examples/otlp/metrics.h
+++ examples/otlp/metrics.h
@@ -1,7 +1,9 @@
 #include "imetricslogger.h"
+#include "otel.h"
 
 namespace metrics
 {
+  nostd::unique_ptr<opentelemetry::metrics::Counter<uint64_t>> meter;
   class MetricsLogger : public IMetricsLogger
 	{
 		public:
@@ -11,3 +13,22 @@ namespace metrics
 	
 			void Log(std::string textStr);
 	};
+
+MetricsLogger::MetricsLogger(std::string fruName, std::string propertyName, std::string propertyDescription, opentelemetry::exporter::otlp::OtlpHttpMetricExporterOptions metrics_opts)
+{
+	otel::InitMetrics(metrics_opts);
+	meter = otel::get_meter(fruName, propertyName, propertyDescription);
+}
+
+MetricsLogger::~MetricsLogger()
+{
+	otel::CleanupMetrics();
+}
+
+MetricsLogger::Log(std::string textStr)
+{
+	meter->Add(1);
+}
+
+
+
