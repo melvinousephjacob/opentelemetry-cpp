636c08fa179d1e53415778acc837998fa568b047
melvinousephjacob <119927690+melvinousephjacob@users.noreply.github.com>
2024-05-29 16:18:12 +0530

    Update otel_metrics.h

--- examples/metrics_simple/otel_metrics.h
+++ examples/metrics_simple/otel_metrics.h
@@ -1,23 +1,15 @@
-#include "opentelemetry/exporters/otlp/otlp_http_exporter_factory.h"
-#include "opentelemetry/exporters/otlp/otlp_http_log_record_exporter_factory.h"
-#include "opentelemetry/exporters/otlp/otlp_http_log_record_exporter_options.h"
-#include "opentelemetry/logs/provider.h"
-#include "opentelemetry/logs/severity.h"
-#include "opentelemetry/sdk/common/global_log_handler.h"
-#include "opentelemetry/sdk/logs/logger_provider_factory.h"
-#include "opentelemetry/sdk/logs/processor.h"
-#include "opentelemetry/sdk/logs/simple_log_record_processor_factory.h"
-#include "opentelemetry/sdk/trace/processor.h"
-#include "opentelemetry/sdk/trace/simple_processor_factory.h"
-#include "opentelemetry/sdk/trace/tracer_provider_factory.h"
-#include "opentelemetry/trace/provider.h"
-#include "iloggingserver.h"
-#include "imetricslogger.h"
-#include <map>
-#include <string>
-#include "opentelemetry/logs/provider.h"
-#include "opentelemetry/sdk/version/version.h"
-#include "opentelemetry/trace/provider.h"
+#include "opentelemetry/exporters/ostream/metric_exporter_factory.h"
+#include "opentelemetry/metrics/provider.h"
+#include "opentelemetry/sdk/metrics/aggregation/default_aggregation.h"
+#include "opentelemetry/sdk/metrics/aggregation/histogram_aggregation.h"
+#include "opentelemetry/sdk/metrics/export/periodic_exporting_metric_reader_factory.h"
+#include "opentelemetry/sdk/metrics/meter.h"
+#include "opentelemetry/sdk/metrics/meter_provider.h"
+#include "opentelemetry/sdk/metrics/meter_provider_factory.h"
+#include "opentelemetry/sdk/metrics/push_metric_exporter.h"
+#include "opentelemetry/sdk/metrics/view/instrument_selector_factory.h"
+#include "opentelemetry/sdk/metrics/view/meter_selector_factory.h"
+#include "opentelemetry/sdk/metrics/view/view_factory.h"
 #include <chrono>
 #include <thread>
 
@@ -45,11 +37,6 @@
 #include <memory>
 #include <thread>
 
-// sdk::TracerProvider and sdk::LoggerProvider is just used to call ForceFlush and prevent to cancel
-// running exportings when destroy and shutdown exporters.It's optional to users.
-#include "opentelemetry/sdk/logs/logger_provider.h"
-#include "opentelemetry/sdk/trace/tracer_provider.h"
-
 #include <iostream>
 #include <string>
 
@@ -74,29 +61,80 @@ namespace internal_log = opentelemetry::sdk::common::internal_log;
 
 namespace otel_metrics
 {
-void InitMetrics(opentelemetry::exporter::otlp::OtlpHttpMetricExporterOptions exporter_options)
+void InitMetrics(const std::string &name)
 {
-  auto exporter = opentelemetry::exporter::otlp::OtlpHttpMetricExporterFactory::Create(exporter_options);
+  auto exporter = exportermetrics::OStreamMetricExporterFactory::Create();
 
   std::string version{"1.2.0"};
   std::string schema{"https://opentelemetry.io/schemas/1.2.0"};
 
   // Initialize and set the global MeterProvider
-  metric_sdk::PeriodicExportingMetricReaderOptions reader_options;
-  reader_options.export_interval_millis = std::chrono::milliseconds(1000);
-  reader_options.export_timeout_millis  = std::chrono::milliseconds(500);
+  metrics_sdk::PeriodicExportingMetricReaderOptions options;
+  options.export_interval_millis = std::chrono::milliseconds(1000);
+  options.export_timeout_millis  = std::chrono::milliseconds(500);
 
   auto reader =
-      metric_sdk::PeriodicExportingMetricReaderFactory::Create(std::move(exporter), reader_options);
+      metrics_sdk::PeriodicExportingMetricReaderFactory::Create(std::move(exporter), options);
 
-  auto context = metric_sdk::MeterContextFactory::Create();
-  context->AddMetricReader(std::move(reader));
+  auto u_provider = metrics_sdk::MeterProviderFactory::Create();
+  auto *p         = static_cast<metrics_sdk::MeterProvider *>(u_provider.get());
 
-  auto u_provider = metric_sdk::MeterProviderFactory::Create(std::move(context));
+  p->AddMetricReader(std::move(reader));
+
+  // counter view
+  std::string counter_name = name + "_counter";
+  std::string unit         = "counter-unit";
+
+  auto instrument_selector = metrics_sdk::InstrumentSelectorFactory::Create(
+      metrics_sdk::InstrumentType::kCounter, counter_name, unit);
+
+  auto meter_selector = metrics_sdk::MeterSelectorFactory::Create(name, version, schema);
+
+  auto sum_view = metrics_sdk::ViewFactory::Create(name, "description", unit,
+                                                   metrics_sdk::AggregationType::kSum);
+
+  p->AddView(std::move(instrument_selector), std::move(meter_selector), std::move(sum_view));
+
+  // observable counter view
+  std::string observable_counter_name = name + "_observable_counter";
+
+  auto observable_instrument_selector = metrics_sdk::InstrumentSelectorFactory::Create(
+      metrics_sdk::InstrumentType::kObservableCounter, observable_counter_name, unit);
+
+  auto observable_meter_selector = metrics_sdk::MeterSelectorFactory::Create(name, version, schema);
+
+  auto observable_sum_view = metrics_sdk::ViewFactory::Create(name, "test_description", unit,
+                                                              metrics_sdk::AggregationType::kSum);
+
+  p->AddView(std::move(observable_instrument_selector), std::move(observable_meter_selector),
+             std::move(observable_sum_view));
+
+  // histogram view
+  std::string histogram_name = name + "_histogram";
+  unit                       = "histogram-unit";
+
+  auto histogram_instrument_selector = metrics_sdk::InstrumentSelectorFactory::Create(
+      metrics_sdk::InstrumentType::kHistogram, histogram_name, unit);
+
+  auto histogram_meter_selector = metrics_sdk::MeterSelectorFactory::Create(name, version, schema);
+
+  auto histogram_aggregation_config = std::unique_ptr<metrics_sdk::HistogramAggregationConfig>(
+      new metrics_sdk::HistogramAggregationConfig);
+
+  histogram_aggregation_config->boundaries_ = std::vector<double>{
+      0.0, 50.0, 100.0, 250.0, 500.0, 750.0, 1000.0, 2500.0, 5000.0, 10000.0, 20000.0};
+
+  std::shared_ptr<metrics_sdk::AggregationConfig> aggregation_config(
+      std::move(histogram_aggregation_config));
+
+  auto histogram_view = metrics_sdk::ViewFactory::Create(
+      name, "description", unit, metrics_sdk::AggregationType::kHistogram, aggregation_config);
+
+  p->AddView(std::move(histogram_instrument_selector), std::move(histogram_meter_selector),
+             std::move(histogram_view));
+
   std::shared_ptr<opentelemetry::metrics::MeterProvider> provider(std::move(u_provider));
-
   metrics_api::Provider::SetMeterProvider(provider);
-}
 
 void CleanupMetrics()
 {
