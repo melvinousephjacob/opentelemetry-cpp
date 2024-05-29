05b3322884f3de1cfff1a465986d679e47fbbc12
melvinousephjacob <119927690+melvinousephjacob@users.noreply.github.com>
2024-05-29 12:22:45 +0530

    Update metrics_main.cc

--- examples/otlp/metrics_main.cc
+++ examples/otlp/metrics_main.cc
@@ -7,6 +7,11 @@ int main()
   std::string metrics_exporter_url = "http://opentelemetry-collector-lwshost:4318/v1/metrics";
 
   auto meter = new metrics::MetricsCounter("Sample_FRU_Name", "Sample_Property_Name", "This is a sample property description", metrics_exporter_url);
-
   meter->Log("Hello");
+
+  auto observableCounter = new metrics::MetricsObservableCounter("Sample_ObservableCounter", "Sample_Property_Name", "This is a sample observable counter", metrics_exporter_url);
+
+  auto observableGauge = new metrics::MetricsObservableGauge("Sample_ObservableGauge", "Sample_Property_Name", "This is a sample observable gauge", metrics_exporter_url);
+
+  auto observableUpDownCounter = new metrics::MetricsObservableUpDownCounter("Sample_ObservableUpDownCounter", "Sample_Property_Name", "This is a sample observable up down counter", metrics_exporter_url);
 }
