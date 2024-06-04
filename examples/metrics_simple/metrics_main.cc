8be6a6aa4f580684f29da00acbbcf48c10e875c7
melvinousephjacob <119927690+melvinousephjacob@users.noreply.github.com>
2024-06-04 11:47:53 +0530

    Update metrics_main.cc

--- examples/metrics_simple/metrics_main.cc
+++ examples/metrics_simple/metrics_main.cc
@@ -4,12 +4,15 @@
 
 int main()
 {
-  auto meter = new metrics::MetricsCounter("Sample_FRU_Name", "Sample_Property_Name", "This is a sample property description");
-  meter->Log("Hello");
+  auto counter = new metrics::MetricsCounter("Sample_FRU_Name", "Sample_Property_Name", "This is a sample property description");
+  counter->Log("Hello");
 
   auto observableCounter = new metrics::MetricsObservableCounter("Sample_ObservableCounter", "Sample_Property_Name", "This is a sample observable counter");
 
   auto observableGauge = new metrics::MetricsObservableGauge("Sample_ObservableGauge", "Sample_Property_Name", "This is a sample observable gauge");
+
+  auto updowncounter = new metrics::MetricsUpDownCounter("Sample_UpDownCounter", "Sample_Property_Name", "This is a sample up down counter");
+  updowncounter->Log("Hello");
 
   auto observableUpDownCounter = new metrics::MetricsObservableUpDownCounter("Sample_ObservableUpDownCounter", "Sample_Property_Name", "This is a sample observable up down counter");
 
