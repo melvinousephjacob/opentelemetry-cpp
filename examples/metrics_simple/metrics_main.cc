53737756aa1050ee4d396c92e11b8316bbee9a22
melvinousephjacob <119927690+melvinousephjacob@users.noreply.github.com>
2024-06-04 15:36:36 +0530

    Update metrics_main.cc

--- examples/metrics_simple/metrics_main.cc
+++ examples/metrics_simple/metrics_main.cc
@@ -4,17 +4,18 @@
 
 int main()
 {
-  auto counter = new metrics::MetricsCounter("Sample_FRU_Name", "Sample_Property_Name", "This is a sample property description");
-  counter->Log("Hello");
+  //auto counter = new metrics::MetricsCounter("Sample_FRU_Name", "Sample_Property_Name", "This is a sample property description");
+  //counter->Log("Hello");
 
   //auto observableCounter = new metrics::MetricsObservableCounter("Sample_ObservableCounter", "Sample_Property_Name", "This is a sample observable counter");
 
   //auto observableGauge = new metrics::MetricsObservableGauge("Sample_ObservableGauge", "Sample_Property_Name", "This is a sample observable gauge");
 
-  auto updowncounter = new metrics::MetricsUpDownCounter("Sample_UpDownCounter", "Sample_Property_Name", "This is a sample up down counter");
-  updowncounter->Log("Hello");
+  //auto updowncounter = new metrics::MetricsUpDownCounter("Sample_UpDownCounter", "Sample_Property_Name", "This is a sample up down counter");
+  //updowncounter->Log("Hello");
 
   //auto observableUpDownCounter = new metrics::MetricsObservableUpDownCounter("Sample_ObservableUpDownCounter", "Sample_Property_Name", "This is a sample observable up down counter");
 
   auto histogram = new metrics::MetricsHistogram("Sample_Histogram", "Sample_Property_Name", "This is a sample histogram");
+  histogram->Log("Hello");
 }
