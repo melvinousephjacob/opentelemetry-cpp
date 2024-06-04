a5a1aa362843f0908c2c3c6edd981961fc2869c9
melvinousephjacob <119927690+melvinousephjacob@users.noreply.github.com>
2024-06-04 17:39:57 +0530

    Update metrics.h

--- examples/metrics_simple/metrics.h
+++ examples/metrics_simple/metrics.h
@@ -17,7 +17,7 @@ namespace metrics
 	
 			~MetricsCounter();
 	
-			static void Log(std::string &textStr);
+			void Log(std::string textStr);
 	};
 
 MetricsCounter::MetricsCounter(std::string fruName, std::string propertyName, std::string propertyDescription)
@@ -31,7 +31,7 @@ MetricsCounter::~MetricsCounter()
 	otel_metrics::CleanupMetrics();
 }
 
-void MetricsCounter::Log(const std::string &textStr)
+void MetricsCounter::Log(std::string textStr)
 {
 	for (uint32_t i = 0; i < 20; ++i)
   	{
@@ -90,7 +90,7 @@ MetricsHistogram::~MetricsHistogram()
 	otel_metrics::CleanupMetrics();
 }
 
-void MetricsHistogram::Log(const std::string &textStr)
+void MetricsHistogram::Log(std::string textStr)
 {
 	auto context           = opentelemetry::context::Context{};
 	for (uint32_t i = 0; i < 20; ++i)
