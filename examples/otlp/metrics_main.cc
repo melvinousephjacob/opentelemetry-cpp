04761b2a036fdf20e62a5e7c6cc813ab1c921f68
melvinousephjacob <119927690+melvinousephjacob@users.noreply.github.com>
2024-07-09 17:24:16 +0530

    Update metrics_main.cc

--- examples/otlp/metrics_main.cc
+++ examples/otlp/metrics_main.cc
@@ -4,14 +4,14 @@
 
 int main()
 {
-  auto observableGauge = new DevicePropertyLogger("Sample_ObservableGauge", "Sample_Property_Name", "This is a sample observable gauge", 30, false, otel_metrics::MeasurementFetcher::Fetcher, metrics_exporter_url);
+  auto observableGauge = new DevicePropertyLogger("Sample_DevicePropertyLogger", "Sample_Property_Name", "This is a sample observable gauge", 30, false, otel_metrics::MeasurementFetcher::Fetcher, metrics_exporter_url);
 
   for (uint32_t i = 0; i < 20; ++i)
   {
     std::this_thread::sleep_for(std::chrono::milliseconds(1000));
   }
 
-  auto histogram = new HistogramLogger("Sample_Histogram", "Sample_Property_Name", 30, 5, 2, 0, 20, "m");
+  auto histogram = new HistogramLogger("Sample_HistogramLogger", "Sample_Property_Name", 30, 5, 2, 0, 20, "m");
   //metrics::MetricsHistogram y;
   //std::thread counter_example{&foo_library::counter_example, name};
   //std::thread observable_counter_example{&foo_library::observable_counter_example, name};
