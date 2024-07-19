fade0ea8bd7ec695c048e00bed7b79742e9edb10
melvinousephjacob <119927690+melvinousephjacob@users.noreply.github.com>
2024-07-19 14:28:27 +0530

    Update metrics_main.cc

--- examples/otlp/metrics_main.cc
+++ examples/otlp/metrics_main.cc
@@ -6,16 +6,16 @@ int main()
 {
   auto observableGauge = new DevicePropertyLogger("Sample_DevicePropertyLogger", "Sample_Property_Name", "This is a sample observable gauge", 30, false, otel_metrics::MeasurementFetcher::Fetcher);
 
-  observableGauge->Log();
+  //observableGauge->Log();
 
-  observableGauge->Log();
+  //observableGauge->Log();
   
   /*for (uint32_t i = 0; i < 20; ++i)
   {
     std::this_thread::sleep_for(std::chrono::milliseconds(1000));
   }*/
 
-  auto histogram = new HistogramLogger("Sample_HistogramLogger", "Sample_Property_Name", 30, 5, 2, 0, 20, "m");
+  //auto histogram = new HistogramLogger("Sample_HistogramLogger", "Sample_Property_Name", 30, 5, 2, 0, 20, "m");
   //metrics::MetricsHistogram y;
   //std::thread counter_example{&foo_library::counter_example, name};
   //std::thread observable_counter_example{&foo_library::observable_counter_example, name};
@@ -24,8 +24,8 @@ int main()
   //counter_example.join();
   //observable_counter_example.join();
   //histogram_example.join();
-  std::vector<long> data{ 10,11,12,13,14,16 };
-  histogram->LogData(data);
+  //std::vector<long> data{ 10,11,12,13,14,16 };
+  //histogram->LogData(data);
 
-  observableGauge->Log();
+  //observableGauge->Log();
 }
