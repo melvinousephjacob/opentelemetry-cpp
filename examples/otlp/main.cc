d439b6a40647128c2e93e3e2598f6427bd9f282b
melvinousephjacob <119927690+melvinousephjacob@users.noreply.github.com>
2024-10-08 11:36:08 +0530

    Update main.cc

--- examples/otlp/main.cc
+++ examples/otlp/main.cc
@@ -8,7 +8,7 @@ int main()
   std::string trace_endpoint_url  = "http://opentelemetry-collector-lwshost:4318/v1/traces";
   std::string log_endpoint_url = "http://opentelemetry-collector-lwshost:4318/v1/logs";
   
-  /*auto centralLogServer1 = new logging::CentralLogServer("Logger #1", "Sample Namespace #1", "Sample Classname #1");
+  auto centralLogServer1 = new logging::CentralLogServer("Logger #1", "Sample Namespace #1", "Sample Classname #1");
   auto centralLogServer2 = new logging::CentralLogServer("Logger #1", "Sample Namespace #2", "Sample Classname #2");
   auto centralLogServer3 = new logging::CentralLogServer("Logger #1", "Sample Namespace #1", "Sample Classname #1");
   
@@ -20,10 +20,10 @@ int main()
   TraceData traceData;
   traceData.AdditionalInfo = "This is some additional info.";
   traceData.DateTime = std::time(0);
-  traceLogger->TraceInfo("This is a sample trace message", traceData);*/
+  traceLogger->TraceInfo("This is a sample trace message", traceData);
 
-  auto deviceinfologger = new logging::DeviceInfo("Sample_file_name");
+  /*auto deviceinfologger = new logging::DeviceInfo("Sample_file_name");
   deviceinfologger->Log("Sample info");
   deviceinfologger->Log("More sample info");
-  deviceinfologger->Log("Even more sample info");
+  deviceinfologger->Log("Even more sample info");*/
 }
